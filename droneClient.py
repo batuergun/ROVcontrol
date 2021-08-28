4fe1771643a8c710271f200a417a759c7d134bdd
Batuhan Ergun <ta6nbe@gmail.com>
2021-08-29 00:32:21 +0300

    class arrangement

--- droneClient.py
+++
@@ -1,9 +0,0 @@
-
-class TcpClient:
-
-    def __init__(self, ip, port, timeout):
-        IPV4= ip
-        #self.socket = socket.create_connection((ip, port), timeout)
-        #self.socket.setblocking(False)
-        #if not socket:
-        #    return None
--- main.py
+++ main.py
@@ -3,14 +3,6 @@ import numpy as np
 
 from imageProcess import Process
 from ROVdrive import Steer
-#from droneClient import TcpClient
-
-#class ROV:
-
-    #def __init__(self):
-
-    #def main(self):
-
 
 if __name__ == '__main__':
 
